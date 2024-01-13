bf5050192208fb005bbffbfd852200329e022adb
sanjeevchandru <chandrusanjeev5@gmail.com>
2024-01-14 00:07:53 +0530

    14-01-p106

---
+++ main.js
@@ -0,0 +1,5 @@
+document.write("70. to rotate the elements left in a given array of integers of length 3."+"<br>");
+function test70(arr){
+    return [arr[1],arr[2],arr[0]];
+}
+document.write("The array :[1,2,3], Ans :"+test70([1,2,3])+"<br><br>");
